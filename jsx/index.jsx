1f847aebc2c0ecd671a47d3540f7b5a174458c19
IvanAndreyshev-NOTEBOOK <vanyha360@yandex.ru>
2017-04-12 16:48:38 +0300

    Убран баг с методом createClass

--- jsx/index.jsx
+++ jsx/index.jsx
@@ -1,10 +1,50 @@
-var Application = React.createClass ({
-    render: function() {
+class Application extends React.Component {
+    render() {
         return (
-        <div className="menu">
-        </div>
-        );
+            <div>
+                <Menu/>
+                <DrawingArea/>
+                <div class="clear"></div>
+            </div>
+        )
     }
-});
+};
+
+class Menu extends React.Component {
+    render() {
+        return (
+            <div className="menu">
+                <h1 class="application_title">Drawing Shape</h1>
+                <ShapesSettings/>
+            </div>
+        )
+    }
+}
+
+class ShapesSettings extends React.Component {
+    render() {
+        return (
+            <form id="shapes_settings" onsubmit="drawShape(); return false;">
+                <SelectShape/>
+            </form>
+        )
+    }
+}
+
+class SelectShape extends React.Component {
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+class DrawingArea extends React.Component {
+    render() {
+        return (
+            <canvas id="drawing_area" className="drawing_area"></canvas>
+        )
+    }
+};
 
 ReactDOM.render(<Application/>, document.getElementById('application_box'));
